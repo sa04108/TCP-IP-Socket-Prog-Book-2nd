49c0fef2bbf9bba4deb4306c816ea5cbd29b8a23
hyunbae <sa04108@naver.com>
2024-03-16 13:46:47 +0900

    Chapter02 연습문제

--- Windows/Chapter02/InitSocket/InitSocket.cpp
+++ Windows/Chapter02/InitSocket/InitSocket.cpp
@@ -1,5 +1,16 @@
 #include "..\..\Common.h"
 
+int f(int x) {
+	if (x >= 0) {
+		WSASetLastError(0);
+		return 0;
+	}
+	else {
+		WSASetLastError(WSAEINVAL);
+		return SOCKET_ERROR;
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	// 윈속 초기화
@@ -8,10 +19,15 @@ int main(int argc, char *argv[])
 		return 1;
 	printf("[알림] 윈속 초기화 성공\n");
 
+	// printf("wVersion: %d, wHighVersion: %d, szDescription: %d, szSystemStatus: %d\n", wsa.wVersion, wsa.wHighVersion, wsa.szDescription, wsa.szSystemStatus);
+
 	// 소켓 생성
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	SOCKET sock = WSASocket(AF_INET, SOCK_DGRAM, 0, NULL, 0, NULL); // UDP
 	if (sock == INVALID_SOCKET) err_quit("socket()");
 	printf("[알림] 소켓 생성 성공\n");
+
+	int retval = f(-100);
+	if (retval == SOCKET_ERROR) err_quit("f()");
 
 	// 소켓 닫기
 	closesocket(sock);
